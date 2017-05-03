e5b78340f4bdf95f6fcd3928eda1eea038e475cb
Daniel Qiu <qsz1328@gmail.com>
2017-05-02 18:11:53 -0700

    source api

--- api/entry.js
+++ api/entry.js
@@ -1,19 +1,24 @@
 'use strict';
-var models = require("../models")
 var rp = require('request-promise-native');
 var config = require('../config')
+var mongojs = require('mongojs')
+
+var db = mongojs(config.DATABASE_URL, ['source', 'entry'])
 
 module.exports = (server) => {
 
     server.get('/source/:id/entry', async (req, res, next) => {
         try {
-            const results = await models.Entry.findAndCountAll({
-                where: { source_id: req.params.id },
-                offset: (req.paginate.page - 1) * req.paginate.per_page,
-                limit: req.paginate.per_page,
-                order: [['published_at', 'DESC'], ['created_at', 'DESC']],
-                include: [{ model: models.Source }, { model: models.Image }]
-            })
+            // const results = await models.Entry.findAndCountAll({
+            //     where: { source_id: req.params.id },
+            //     offset: (req.paginate.page - 1) * req.paginate.per_page,
+            //     limit: req.paginate.per_page,
+            //     order: [['published_at', 'DESC'], ['created_at', 'DESC']],
+            //     include: [{ model: models.Source }, { model: models.Image }]
+            // })
+
+
+
 
             res.charSet('utf-8');
             res.paginate.send(results.rows, results.count);
--- api/source.js
+++ api/source.js
@@ -1,44 +1,47 @@
 'use strict';
 var models = require("../models")
-var rssspider = require("../spider/rss")
+// var rssspider = require("../spider/rss")
+var config = require('../config')
+var mongojs = require('mongojs')
+var db = mongojs(config.DATABASE_URL, ['source', 'entry'])
 
 module.exports = (server) => {
 
     server.get('/source', async (req, res, next) => {
         try {
-            const results = await models.Source.findAndCountAll({
-                offset: (req.paginate.page - 1) * req.paginate.per_page,
-                limit: req.paginate.per_page,
-                order: [['updated_at', 'DESC']]
+            var offset = (req.paginate.page - 1) * req.paginate.per_page
+            var limit = req.paginate.per_page
+            db.source.count(null, (err, count) => {
+                db.source.find().sort({ 'updated_at': -1 }).skip(offset).limit(limit, (err, results) => {
+                    res.charSet('utf-8');
+                    res.paginate.send(results, count);
+                })
             })
-            res.charSet('utf-8');
-            res.paginate.send(results.rows, results.count);
-
         } catch (error) {
             res.send({ 'code': 'failed', 'message': error.message })
         }
     });
 
-    server.post("/rss-source", async (req, res, next) => {
-        if (req.body == null) {
-            res.send({ 'code': 'failed', 'message': 'cannot parse request body.' })
-        }
-        else {
-            var rss = {
-                name: req.body.name,
-                image: req.body.image,
-                link: req.body.link,
-                description: req.body.description,
-                type: 'rss'
-            }
+    // server.post("/rss-source", async (req, res, next) => {
+    //     if (req.body == null) {
+    //         res.send({ 'code': 'failed', 'message': 'cannot parse request body.' })
+    //     }
+    //     else {
+    //         var rss = {
+    //             name: req.body.name,
+    //             image: req.body.image,
+    //             link: req.body.link,
+    //             description: req.body.description,
+    //             type: 'rss'
+    //         }
 
-            try {
-                await rssspider.saveRSSSource(rss);
-                res.send({ 'code': 'success' })
-            } catch (error) {
-                res.send({ 'code': 'failed', 'message': error.message })
-            }
-        }
-    })
+    //         try {
+    //             await rssspider.saveRSSSource(rss);
+    //             res.send({ 'code': 'success' })
+    //         } catch (error) {
+    //             res.send({ 'code': 'failed', 'message': error.message })
+    //         }
+    //     }
+    // })
 };
 
