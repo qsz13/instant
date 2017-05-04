75d792244dfb49710feb5cbda0334f66fd8a1b7c
Daniel Qiu <qsz1328@gmail.com>
2017-05-04 01:36:41 -0700

    source entry api

--- api/entry.js
+++ api/entry.js
@@ -13,10 +13,11 @@ module.exports = (server) => {
             var offset = (req.paginate.page - 1) * req.paginate.per_page
             var limit = req.paginate.per_page
             Entry.count(null, (err, count) => {
-                Entry.find(null, { __v: 0, type: 0, updatedAt: 0 }).populate('source', { type: 0, link: 0, updatedAt: 0, createdAt: 0 }).sort({ 'createdAt': -1 }).skip(offset).limit(limit).exec((err, results) => {
-                    res.charSet('utf-8');
-                    res.paginate.send(results, count);
-                })
+                Entry.find(null, { __v: 0, type: 0, updatedAt: 0 }).populate('source', { type: 0, link: 0, updatedAt: 0, createdAt: 0 })
+                    .sort({ 'createdAt': -1 }).skip(offset).limit(limit).exec((err, results) => {
+                        res.charSet('utf-8');
+                        res.paginate.send(results, count);
+                    })
             })
         } catch (error) {
             res.send({ 'code': 'failed', 'message': error.message })
@@ -26,19 +27,16 @@ module.exports = (server) => {
 
     server.get('/source/:id/entry', async (req, res, next) => {
         try {
-            // const results = await models.Entry.findAndCountAll({
-            //     where: { source_id: req.params.id },
-            //     offset: (req.paginate.page - 1) * req.paginate.per_page,
-            //     limit: req.paginate.per_page,
-            //     order: [['published_at', 'DESC'], ['created_at', 'DESC']],
-            //     include: [{ model: models.Source }, { model: models.Image }]
-            // })
-
-
-
-
-            res.charSet('utf-8');
-            res.paginate.send(results.rows, results.count);
+            var offset = (req.paginate.page - 1) * req.paginate.per_page
+            var limit = req.paginate.per_page
+            Entry.count({ source: req.params.id }, (err, count) => {
+                Entry.find({ source: req.params.id }, { __v: 0, type: 0, updatedAt: 0 })
+                    .populate('source', { type: 0, link: 0, updatedAt: 0, createdAt: 0 })
+                    .sort({ 'createdAt': -1 }).skip(offset).limit(limit).exec((err, results) => {
+                        res.charSet('utf-8');
+                        res.paginate.send(results, count);
+                    })
+            })
         } catch (error) {
             res.send({ 'code': 'failed', 'message': error.message })
         }
